7a6e29835e2278734d9700f4fa6324af75740af4
PengKailong <aduuu@163.com>
2019-05-09 23:38:42 +0800

    DAOs Updated

--- src/main/java/shu/ces/filmsystem/DAO/AdministratorDAO.java
+++ src/main/java/shu/ces/filmsystem/DAO/AdministratorDAO.java
@@ -1,6 +1,7 @@
 package shu.ces.filmsystem.DAO;
 
 import shu.ces.filmsystem.Model.Administrator;
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -8,107 +9,113 @@ import shu.ces.filmsystem.DAO.BaseDAO;
 
 
 public class AdministratorDAO {
-	PreparedStatement pstmt;
+    PreparedStatement pstmt;
 
-	public boolean insertAdministrator(Administrator admin) {
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement("insert into administrator (username, password, img) values(?,?,?)");
-			pstmt.setString(1, admin.getUsername());
-			pstmt.setString(2, admin.getPassword());
-			pstmt.setString(3, admin.getImg());
-			pstmt.executeUpdate();
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public boolean insertAdministrator(Administrator admin) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("insert into administrator (username, password, img) values(?,?,?)");
+            pstmt.setString(1, admin.getUsername());
+            pstmt.setString(2, admin.getPassword());
+            pstmt.setString(3, admin.getImg());
+            pstmt.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	public boolean deleteAdministrator(int id) {
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement("delete from administrator where id=?");
-			pstmt.setInt(1, id);
-			int i = pstmt.executeUpdate();
-			if (i == 1)
-				return true;
-			else
-				return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public ArrayList<Administrator> searchForList(){
-		ArrayList<Administrator> al = new ArrayList<Administrator>();
-		try {
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				Administrator admin = new Administrator();
-				admin.setId(rs.getInt(1));
-				admin.setUsername(rs.getString(2));
-				admin.setPassword(rs.getString(3));
-				admin.setImg(rs.getString(4));
-				al.add(admin);
-			}
-			return al;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    public boolean deleteAdministrator(int id) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("delete from administrator where id=?");
+            pstmt.setInt(1, id);
+            int i = pstmt.executeUpdate();
+            if (i == 1)
+                return true;
+            else
+                return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	public ArrayList<Administrator> searchAdministrator(String username, String password) {
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement("select * from administrator where username=? and password=?");
-			pstmt.setString(1, username);
-			pstmt.setString(2, password);
-			return searchForList();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    public ArrayList<Administrator> searchForList() {
+        ArrayList<Administrator> al = new ArrayList<Administrator>();
+        try {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Administrator admin = new Administrator();
+                admin.setId(rs.getInt(1));
+                admin.setUsername(rs.getString(2));
+                admin.setPassword(rs.getString(3));
+                admin.setImg(rs.getString(4));
+                al.add(admin);
+            }
+            return al;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	public String getAdministratorName(int id) {
-		String username = null;
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement("select username from administrator where id=?");
-			pstmt.setInt(1, id);
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				username = rs.getString(1);
-			}
-			return username;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    public ArrayList<Administrator> searchAdministratorById(int id) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("select * from administrator where id=?");
+            pstmt.setInt(1, id);
+            return searchForList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	public boolean updateAdministrator(Administrator admin) {
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement(
-					"update administrator set username=?,password=?,img=? where id=?");
-			pstmt.setString(1, admin.getUsername());
-			pstmt.setString(2, admin.getPassword());
-			pstmt.setString(3, admin.getImg());
-			pstmt.setInt(4, admin.getId());
-			pstmt.executeUpdate();
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public ArrayList<Administrator> searchAdministratorByName(String username) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("select * from administrator where username=?");
+            pstmt.setString(1, username);
+            return searchForList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ArrayList<Administrator> listAllAdministrator() {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("select * from administrator");
+            return searchForList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean updateAdministrator(Administrator admin) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement(
+                    "update administrator set username=?,password=?,img=? where id=?");
+            pstmt.setString(1, admin.getUsername());
+            pstmt.setString(2, admin.getPassword());
+            pstmt.setString(3, admin.getImg());
+            pstmt.setInt(4, admin.getId());
+            pstmt.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/main/java/shu/ces/filmsystem/DAO/CinemaDAO.java
+++ src/main/java/shu/ces/filmsystem/DAO/CinemaDAO.java
@@ -8,138 +8,121 @@ import shu.ces.filmsystem.Model.Film;
 import shu.ces.filmsystem.Model.Cinema;
 
 public class CinemaDAO {
-	PreparedStatement pstmt;
+    PreparedStatement pstmt;
 
-	public boolean insertCinema(Cinema cinema) {
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement(
-					"insert into cinema (username, password, img, city, address) values(?,?,?,?,?)");
-			pstmt.setString(1, cinema.getUsername());
-			pstmt.setString(2, cinema.getPassword());
-			pstmt.setString(3, cinema.getImg());
-			pstmt.setString(4, cinema.getCity());
-			pstmt.setString(5, cinema.getAddress());
-			pstmt.executeUpdate();
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public boolean insertCinema(Cinema cinema) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement(
+                    "insert into cinema (username, password, img, city, address) values(?,?,?,?,?)");
+            pstmt.setString(1, cinema.getUsername());
+            pstmt.setString(2, cinema.getPassword());
+            pstmt.setString(3, cinema.getImg());
+            pstmt.setString(4, cinema.getCity());
+            pstmt.setString(5, cinema.getAddress());
+            pstmt.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	public boolean deleteCinema(int id) {
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement("delete from cinema where id=?");
-			pstmt.setInt(1, id);
-			int i = pstmt.executeUpdate();
-			if (i == 1)
-				return true;
-			else
-				return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public boolean deleteCinema(int id) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("delete from cinema where id=?");
+            pstmt.setInt(1, id);
+            int i = pstmt.executeUpdate();
+            if (i == 1)
+                return true;
+            else
+                return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	public ArrayList<Cinema> searchForList() {
-		ArrayList<Cinema> al = new ArrayList<Cinema>();
-		try {
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				Cinema cinema = new Cinema();
-				cinema.setId(rs.getInt(1));
-				cinema.setUsername(rs.getString(2));
-				cinema.setPassword(rs.getString(3));
-				cinema.setImg(rs.getString(4));
-				cinema.setCity(rs.getString(5));
-				cinema.setAddress(rs.getString(6));
-				al.add(cinema);
-			}
-			return al;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-//	public Cinema checkCinema(String username, String password) {
-//		try {
-//			BaseDAO BD = new BaseDAO();
-//			Connection ct = BD.getConnection();
-//			pstmt = ct.prepareStatement("select * from cinema where username=? and password=?");
-//			pstmt.setString(1, username);
-//			pstmt.setString(2, password);
-//			ResultSet rs = pstmt.executeQuery();
-//			while (rs.next()) {
-//				Cinema c = new Cinema();
-//				c.setId(rs.getInt(1));
-//				c.setUsername(rs.getString(2));
-//				c.setPassword(rs.getString(3));
-//				c.setImg(rs.getString(4));
-//				c.setCity(rs.getString(5));
-//				c.setAddress(rs.getString(6));
-//				return c;
-//			}
-//			return null;
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return null;
-//		}
-//	}
+    public ArrayList<Cinema> searchForList() {
+        ArrayList<Cinema> al = new ArrayList<Cinema>();
+        try {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Cinema cinema = new Cinema();
+                cinema.setId(rs.getInt(1));
+                cinema.setUsername(rs.getString(2));
+                cinema.setPassword(rs.getString(3));
+                cinema.setImg(rs.getString(4));
+                cinema.setCity(rs.getString(5));
+                cinema.setAddress(rs.getString(6));
+                al.add(cinema);
+            }
+            return al;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-//	public Cinema searchCinemaById(int id) {
-//		Cinema cinema = null;
-//		try {
-//			BaseDAO BD = new BaseDAO();
-//			Connection ct = BD.getConnection();
-//			pstmt = ct.prepareStatement("select * from cinema where id=?");
-//			pstmt.setInt(1, id);
-//			ResultSet rs = pstmt.executeQuery();
-//			while (rs.next()) {
-//				username = rs.getString(1);
-//			}
-//			return username;
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return null;
-//		}
-//	}
+    public ArrayList<Cinema> searchCinemaById(int id) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("select * from cinema where id=?");
+            pstmt.setInt(1, id);
+            return searchForList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	public ArrayList<Cinema> listAllCinema() {
-		ArrayList<Cinema> al = new ArrayList<Cinema>();
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement("select * from cinema");
-			return searchForList();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    public ArrayList<Cinema> searchCinemaByName(String name) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("select * from cinema where username=?");
+            pstmt.setString(1, name);
+            return searchForList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	public boolean updateCinema(Cinema cinema) {
-		try {
-			BaseDAO BD = new BaseDAO();
-			Connection ct = BD.getConnection();
-			pstmt = ct.prepareStatement(
-					"update cinema set username=?,password=?,img=?,city=?,address=? where id=?");
-			pstmt.setString(1, cinema.getUsername());
-			pstmt.setString(2, cinema.getPassword());
-			pstmt.setString(3, cinema.getImg());
-			pstmt.setString(4, cinema.getCity());
-			pstmt.setString(5, cinema.getAddress());
-			pstmt.setInt(6, cinema.getId());
-			pstmt.executeUpdate();
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public ArrayList<Cinema> listAllCinema() {
+        ArrayList<Cinema> al = new ArrayList<Cinema>();
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement("select * from cinema");
+            return searchForList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean updateCinema(Cinema cinema) {
+        try {
+            BaseDAO BD = new BaseDAO();
+            Connection ct = BD.getConnection();
+            pstmt = ct.prepareStatement(
+                    "update cinema set username=?,password=?,img=?,city=?,address=? where id=?");
+            pstmt.setString(1, cinema.getUsername());
+            pstmt.setString(2, cinema.getPassword());
+            pstmt.setString(3, cinema.getImg());
+            pstmt.setString(4, cinema.getCity());
+            pstmt.setString(5, cinema.getAddress());
+            pstmt.setInt(6, cinema.getId());
+            pstmt.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
